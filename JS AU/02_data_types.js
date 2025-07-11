ed220c711377ba38cf69d99cc7a8180f06385845
priyashu-code <priyanshu.28bisht@gmail.com>
2025-07-11 12:10:31 +0530

    learned about data types

---
+++ JS AU/02_data_types.js
@@ -0,0 +1,21 @@
+/*
+"use strict"; it treats the whole code as newer version of js
+evem old ones
+*/
+
+"use strict";
+
+// alert("Hello"); //it only works in browser or when js file is bind with HTML
+/*
+There is a different ways to use alert in node js will see it later
+*/
+
+//Use MDN to study about things
+
+// NUll is a representation of empty value
+// we can assign this null if we dont't want to keep it undefined
+
+console.log(typeof null);
+// So js treat null as an object
+console.log(typeof undefined);
+// unndefined is undefined
