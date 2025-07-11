322502c78af028c860c2b186bfb819116cdbc446
priyashu-code <priyanshu.28bisht@gmail.com>
2025-07-11 11:13:29 +0530

    timer

---
+++ JS kirat/Basics_js/timer.js
@@ -0,0 +1,10 @@
+function greet(){
+    console.log("Hello")
+
+}
+
+// setTimeout(greet, 3*1000);
+// So after 3 sec it will print Hello
+
+setInterval(greet,1*1000);
+// So after 1 sec it will call greet
